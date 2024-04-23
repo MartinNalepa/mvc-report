a7946577beca08ef3874689dc28a62ae59d20644
Martin Nalepa <apelansign@gmail.com>
2024-04-23 15:15:12 +0200

    Refactor generation of id and name from Game class to Player class

--- src/Entity/Card/Game.php
+++ src/Entity/Card/Game.php
@@ -4,27 +4,51 @@ namespace App\Entity\Card;
 
 use App\Entity\Card\Exceptions\EmptyDeckException;
 
-abstract class Game {
+/**
+ * Abstract parent class for all game classes, representing a card game.
+ */
+abstract class Game
+{
     protected $players;
     protected $deck;
 
-    public function __construct() {
+    /**
+     * Creates a new card game with an empty list of players and a default deck of cards.
+     */
+    public function __construct()
+    {
         $this->players = [];
         $this->deck = new FrenchDeckNoJoker();
     }
 
-    public function setDeck(DeckOfCards $deck): void{
+    /**
+     * Sets deck of cards for the game.
+     * @param DeckOfCards $deck The deck of cards to use for the game.
+     */
+    public function setDeck(DeckOfCards $deck): void
+    {
         $this->deck = $deck;
     }
 
-    public function getDeck(): DeckOfCards {
+    /**
+     * Returns the deck of cards for the gaem.
+     * @return DeckOfCards The deck of cards for the game.
+     */
+    public function getDeck(): DeckOfCards
+    {
         return $this->deck;
     }
 
-    public function drawCards(int $numCards = 1): array {
+    /**
+     * Draws a number of cards from the deck and returns them.
+     * @param int $numCards The number of cards to draw from the deck.
+     * @return array An array containing the drawn cards and a boolean indicating if the deck is empty.
+     */
+    public function drawCards(int $numCards = 1): array
+    {
         $cards = [];
         $deckEmpty = false;
-    
+
         for ($i = 0; $i < $numCards; $i++) {
             try {
                 $cards[] = $this->deck->drawCard();
@@ -33,49 +57,73 @@ abstract class Game {
                 break;
             }
         }
-    
+
         return [
             'cards' => $cards,
             'deckEmpty' => $deckEmpty
         ];
     }
-    
 
-    public function deal(Player $player, $numCards): void {
+
+    /**
+     * Deals a number of cards from the deck to a player.
+     * @param Player $player The player to deal the cards to.
+     * @param int $numCards The number of cards to deal to the player.
+     * @return void
+     */
+    public function deal(Player $player, $numCards): void
+    {
         for ($i = 0; $i < $numCards; $i++) {
             $player->addCardToHand($this->deck->drawCard());
         }
     }
 
-    public function addPlayer(String $playerId = null, String $playerName = null, String $playerType = 'human'): void{
-        if ($playerId === null) {
-            $prefix = $playerType === 'human' ? 'p_' : 'b_';
-            $playerId = uniqid($prefix, true);
-        }
-        if ($playerName === null) {
-            $count = count($this->players) + 1;
-            $playerName = $playerType === 'human' ? "Player {$count}" : "Bot {$count}";
-        }
+    /**
+     * Adds a player to the game.
+     * @param string $playerId The ID of the player (default to set unique id).
+     * @param string $playerName The name of the player (defaults to Player or Bot, based on type).
+     * @param string $playerType The type of the player (human or bot).
+     * @return void
+     */
+    public function addPlayer(String $playerId = null, String $playerName = null, String $playerType = 'human'): void
+    {
         $this->players[] = new Player($playerId, $playerName, $playerType);
     }
 
-    public function getPlayers(): array {
+    /**
+     * Returns the players in the game.
+     * @return array The players in the game.
+     */
+    public function getPlayers(): array
+    {
         return $this->players;
     }
 
-    public function resetPlayers(): void {
+    /**
+     * Removes all players from the game.
+     */
+    public function resetPlayers(): void
+    {
         $this->players = [];
     }
 
-    public function __toArray() {
+    /**
+     * Returns the game as serializable array.
+     * @return array An array with the deck and players in the game.
+     */
+    public function __toArray()
+    {
         return [
             'deck' => $this->deck->__toArray(),
-            'players' => array_map(function($player) {
+            'players' => array_map(function ($player) {
                 return $player->__toArray();
             }, $this->players)
         ];
     }
 
+    /**
+     * Initializes the game.
+     */
     abstract public function initializeGame();
     // abstract public function playTurn(Player $player);
     // abstract public function endGame();
--- src/Entity/Card/Player.php
+++ src/Entity/Card/Player.php
@@ -2,50 +2,124 @@
 
 namespace App\Entity\Card;
 
-class Player {
+/**
+ * Class representing a player in a card game.
+ */
+class Player
+{
     private $id;
     private $name;
     private $hand;
     private $playerType;
 
-    public function __construct(String $id, String $name, $playerType = 'human') {
-        $this->id = $id;
-        $this->name = $name;
+    /**
+     * Construct a Player object with a unique identifier, name, and player type.
+     * If no ID or name is provided, they are generated based on the player type.
+     *
+     * @param string|null $id The unique identifier for the player. If null, a unique ID is generated.
+     * @param string|null $name The name of the player. If null, a default name is assigned based on player type.
+     * @param string $playerType The type of the player (default to 'human'). Can be 'human' or 'bot'.
+     */
+    public function __construct(?string $id = null, ?string $name = null, string $playerType = 'human')
+    {
         $this->playerType = $playerType;
+        $this->id = $id ?? $this->generateId();
+        $this->name = $name ?? $this->generateName();
         $this->hand = new CardHand();
     }
 
-    public function addCardToHand(Card $card) {
+    /**
+     * Generates a unique identifier for the player.
+     * @return string A unique identifier for the player.
+     */
+    private function generateId(): string
+    {
+        $prefix = $this->playerType === 'human' ? 'p_' : 'b_';
+        return uniqid($prefix, true);
+    }
+
+    /**
+     * Generates a default name for the player based on the player type.
+     * @return string A default name for the player.
+     */
+    private function generateName(): string
+    {
+        return $this->playerType === 'human' ? "Player" : "Bot";
+    }
+
+    /**
+     * Adds a card to the player's hand.
+     * @param Card $card The card to add to the player's hand.
+     * @return void
+     */
+    public function addCardToHand(Card $card): void
+    {
         $this->hand->addCard($card);
     }
 
-    public function playCard(Card $card) {
+    /**
+     * Plays a card from the player's hand.
+     * @param Card $card The card to play from the player's hand.
+     * @return Card|null The card that was played, or null if the card was not found.
+     */
+    public function playCard(Card $card): ?Card
+    {
         return $this->hand->takeCard($card);
     }
 
-    public function playAllCards() {
+    /**
+     * Plays all cards from the player's hand.
+     * @return array An array of cards that were played.
+     */
+    public function playAllCards(): array
+    {
         $cards = $this->hand->getCards();
         $this->hand = new CardHand();
         return $cards;
     }
 
-    public function getName() {
+    /**
+     * Returns player name
+     * @return string The name of the player.
+     */
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getId() {
+    /**
+     * Returns player id.
+     * @return string The id of the player.
+     */
+    public function getId(): string
+    {
         return $this->id;
     }
 
-    public function getHand() {
+    /**
+     * Returns cards in the player's hand.
+     * @return array The cards in the player's hand.
+     */
+    public function getHand(): array
+    {
         return $this->hand->getCards();
     }
 
-    public function getPlayerType() {
+    /**
+     * Returns the type of the player.
+     * @return string The type of the player ('human' or 'bot').
+     */
+    public function getPlayerType(): string
+    {
         return $this->playerType;
     }
 
-    public function __toArray() {
+    /**
+     * Returns the player as a serializable array.
+     * @return array An array with the player's id, name, and hand.
+     */
+    public function __toArray(): array
+    {
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -53,7 +127,11 @@ class Player {
         ];
     }
 
-    public function clearHand() {
+    /**
+     * Clears the player's hand.
+     */
+    public function clearHand(): void
+    {
         $this->hand = new CardHand();
     }
 }
